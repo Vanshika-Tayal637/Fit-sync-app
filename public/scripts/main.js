cee4b207b6d878d10f49828c6a4f56d9a2655df9
lovish1113 <lovishdeepsingh@student.adelaide.edu.au>
2025-05-10 06:15:56 +0000

    save

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,13 @@
+// eslint-disable-next-line no-unused-vars
 let workoutSelected='';
+function showSections(section_name){
+    const sections_list=['home','workouts','tracker'];
+    for (let i of sections_list){
+        document.getElementById(i).classList.add('hidden');
+
+    }
+    document.getElementById(section_name).classList.remove('hidden');
+}
 function login(){
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
@@ -7,15 +16,8 @@ function login(){
         document.getElementById('main-screen').classList.remove('hidden');
         showSections('home');
     }else{
+        // eslint-disable-next-line no-alert
         alert("Enter a valid username and password!");
     }
 }
 
-function showSections(section_name){
-    const sections_list=['home','workouts','tracker'];
-    for (let i of sections_list){
-        document.getElementById(i).classList.add('hidden');
-
-    };
-    document.getElementById(section_name).classList.remove('hidden');
-}
