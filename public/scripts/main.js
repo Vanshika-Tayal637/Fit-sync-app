869e584c310c62e70434feb57e2f395a77a2043a
lovish1113 <lovishdeepsingh@student.adelaide.edu.au>
2025-05-10 06:12:06 +0000

    a

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,21 @@
-...
+let workoutSelected='';
+function login(){
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    if(username&&password){
+        document.getElementById('login').classList.add('hidden');
+        document.getElementById('main-screen').classList.remove('hidden');
+        showSections('home');
+    }else{
+        alert("Enter a valid username and password!");
+    }
+}
+
+function showSections(section_name){
+    const sections_list=['home','workouts','tracker'];
+    for (let i of sections_list){
+        document.getElementById(i).classList.add('hidden');
+
+    };
+    document.getElementById(section_name).classList.remove('hidden');
+}
